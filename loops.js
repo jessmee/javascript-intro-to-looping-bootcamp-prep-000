e17a931288522b6b6ed3c8aa134065999b5b0550
Jessica Yoo <jessmee1234@gmail.com>
2018-02-22 23:17:01 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,7 +16,7 @@ function whileLoop(n) {
  return "done"
 }
 function doWhileLoop(array) {
-  while (array.length > 0 && maybeTrue()){
+  do { (array.length > 0 && maybeTrue()){
     return array
   }
 }
